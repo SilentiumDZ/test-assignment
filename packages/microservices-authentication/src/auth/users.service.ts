765690539b054210c12e4ac91474f5b8d0d4b760
Daniil Taranenko <silentiumdz@gmail.com>
2020-09-24 19:29:41 +0300

    initial commit

--- packages/microservices-authentication/src/auth/users.service.ts
+++ packages/microservices-authentication/src/auth/users.service.ts
@@ -1,27 +1,43 @@
 import { Injectable } from '@nestjs/common';
 import * as fs from 'fs';
 
-export type User = any;
+export type User = { username: string; password: string };
 
 @Injectable()
 export class UsersService {
-  private readonly users: User[] = [];
+  private readonly usersFilePath = 'users.json';
 
   async findUser(username: string): Promise<User | undefined> {
-    const rawData = fs.readFileSync('users.json');
-    const parsedData = JSON.parse(rawData.toString());
+    const users = this.getUsers();
 
-    for (const item of parsedData) {
-      if (item.code === username) {
-        return true;
+    for (const item of users) {
+      if (item.username === username) {
+        return item;
       }
     }
   }
 
   async addUser(username, password) {
+    const users = this.getUsers();
+
     const user = { username, password };
 
-    const data = JSON.stringify(user);
-    fs.writeFileSync('users.json', data);
+    users.push(user);
+
+    const data = JSON.stringify(users);
+    fs.writeFileSync(this.usersFilePath, data);
+  }
+
+  private getUsers(): User[] {
+    const users = [];
+
+    try {
+      const rawData = fs.readFileSync(this.usersFilePath);
+      users.push(...JSON.parse(rawData.toString()) as User[]);
+    } catch (error) {
+      throw new Error('Error reading users from file');
+    }
+
+    return users;
   }
 }
